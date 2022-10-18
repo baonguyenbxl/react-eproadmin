c84cf8584d28c7963a59757969f39516f00b2e6d
baonguyenbxl <69773267+baonguyenbxl@users.noreply.github.com>
2022-10-18 22:37:23 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Banner } from "./header/banner/banner";
+function App() {
+  return (
+    <div className="App">
+<Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header/banner/banner.js
@@ -0,0 +1,7 @@
+import "./banner.css"
+export const Banner = () =>
+{
+  return ( <div className="bannerdiv">
+    <text className="bannertext"><b>e-Pro Greffe</b></text>
+  </div>)
+}
